b771cff8d98cce841b9ba7ec3f8072231c6be9d4
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-09-04 00:35:19 -0500

    updated song & job models

--- music-management/src/main/java/com/music/controller/JobController.java
+++ music-management/src/main/java/com/music/controller/JobController.java
@@ -4,14 +4,19 @@ import com.music.model.Job;
 import com.music.model.Song;
 import com.music.repository.JobRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@Controller
 public class JobController {
     @Autowired
     JobRepository jobRepository;
-    
+
+
+
+
     //Get the form
     @GetMapping("/list")
     public String loadJobPage(Model model) {
@@ -24,4 +29,5 @@ public class JobController {
         model.addAttribute("jobs", jobRepository.findAll());
         return "list";
     }
+
 }
