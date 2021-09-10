c05ee52a7d845497caeb3d057b06c0a67e9211b1
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-09-10 01:24:34 -0500

    added course model

---
+++ music-management/src/main/java/com/music/model/Course.java
@@ -0,0 +1,84 @@
+package com.music.model;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @NotNull
+    @Size(min = 3)
+
+    private String title;
+
+    @NotNull
+    @Size(min = 3)
+    private String instructor;
+
+    @NotNull
+    @Size(min = 10)
+    private String description;
+
+
+    @NotNull
+    @Size(min = 3)
+    private int credit;
+
+
+    public Course() {
+    }
+
+    public Course(long id, String title, String instructor, String description, int credit) {
+        this.id = id;
+        this.title = title;
+        this.instructor = instructor;
+        this.description = description;
+        this.credit = credit;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getCredit() {
+        return credit;
+    }
+
+    public void setCredit(int credit) {
+        this.credit = credit;
+    }
+}
