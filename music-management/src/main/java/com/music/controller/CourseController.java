22ba79e0182a780b5e2a155e03492cb6a76c3a70
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-09-11 00:09:27 -0500

    added courses & nav fragment

--- music-management/src/main/java/com/music/controller/CourseController.java
+++ music-management/src/main/java/com/music/controller/CourseController.java
@@ -1,4 +1,41 @@
 package com.music.controller;
 
+import com.music.model.Course;
+import com.music.repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class CourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+
+    @GetMapping("/courses")
+    public String list(){
+        return  "courses/list";
+    }
+
+    @RequestMapping("/courses")
+    public String listCourses(Model model){
+        model.addAttribute("courses", courseRepository.findAll());
+        return "courses/list";
+
+    }
+
+
+   //Add course
+    @GetMapping("/courses/add")
+   public String courseForm(Model model){
+        model.addAttribute("course", new Course());
+        return "courses/courseform";
+
+   }
+
+
+
 }
--- music-management/src/main/java/com/music/model/Course.java
+++ music-management/src/main/java/com/music/model/Course.java
@@ -1,11 +1,14 @@
 package com.music.model;
 
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+
+@Entity
 public class Course {
 
     @Id
