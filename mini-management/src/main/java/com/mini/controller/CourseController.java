064473e92e279e40b78a3ac22801dd93580ed16a
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-09-14 12:39:07 -0500

    updated course controller

--- mini-management/src/main/java/com/mini/controller/CourseController.java
+++ mini-management/src/main/java/com/mini/controller/CourseController.java
@@ -2,11 +2,13 @@ package com.mini.controller;
 
 import com.mini.model.Course;
 import com.mini.repository.CourseRepository;
+import org.dom4j.rule.Mode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -51,5 +53,29 @@ public class CourseController {
         return "redirect:/courses/list";
     }
 
+
+
+    //Show course Details
+    @RequestMapping("/courses/details/{id}")
+    public  String showCourse(@PathVariable("id") long id, Model model){
+        model.addAttribute("course", courseRepository.findById(id).get());
+        return "courses/show";
+
+    }
+
+    //Update Course
+    @RequestMapping("/courses/update/{id}")
+    public String updateCourse(@PathVariable("id") long id, Model model){
+        model.addAttribute("course", courseRepository.findById(id).get());
+        return "courses/list";
+    }
+
+
+    //Delete Course
+    @RequestMapping("/courses/delete/{id}")
+    public String deleteCourse(@PathVariable("id") long id){
+        courseRepository.deleteById(id);
+        return "courses/courseform";
+    }
 
 }
