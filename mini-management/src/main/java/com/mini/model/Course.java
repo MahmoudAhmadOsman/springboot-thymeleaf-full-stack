6e4a2fef9e30701bac81aaaec85fb514207f52f2
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-09-15 11:18:04 -0500

    update courses crud

--- mini-management/src/main/java/com/mini/model/Course.java
+++ mini-management/src/main/java/com/mini/model/Course.java
@@ -1,6 +1,7 @@
 package com.mini.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -23,7 +24,8 @@ public class Course {
     private String instructor;
 
     @NotNull
-    @Size(min = 10)
+    @Size(min = 20)
+    @Lob
     private String description;
 
 
