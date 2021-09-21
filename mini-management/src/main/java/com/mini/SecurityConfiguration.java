29e437551e5126e4a62772b2d52c9d99c924e456
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-09-20 21:49:31 -0500

    added spring boot security configuration

---
+++ mini-management/src/main/java/com/mini/SecurityConfiguration.java
@@ -0,0 +1,35 @@
+package com.mini;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected  void configure(HttpSecurity htttp) throws Exception{
+        htttp.authorizeRequests().anyRequest().authenticated()
+                .and().formLogin();
+    }
+
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication().withUser("user")
+                .password(passwordEncoder().encode("password")).authorities("USER");
+    }
+
+
+
+
+}
