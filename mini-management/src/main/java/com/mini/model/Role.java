54e5b7e7451e9400b06aa398f6b0080e2febfed0
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-09-28 00:22:52 -0500

    added role model, role repository & user repository

--- mini-management/src/main/java/com/mini/model/Role.java
+++ mini-management/src/main/java/com/mini/model/Role.java
@@ -1,12 +1,18 @@
 package com.mini.model;
 
+import javax.persistence.*;
 import java.util.Collection;
 
+@Entity
 public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private long id;
+
+    @Column(unique = true)
     private String role;
 
-
+    @ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
     private Collection<User> users;
 
 
