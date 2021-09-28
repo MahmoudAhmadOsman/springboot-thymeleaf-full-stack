edf049564a462afde55321ffc22102bbb23fdd5e
Mahmoud Osman <mahmoudosman.som@gmail.com>
2021-09-27 23:30:43 -0500

    added User model

---
+++ mini-management/src/main/java/com/mini/model/User.java
@@ -0,0 +1,18 @@
+package com.mini.model;
+
+
+
+public class User {
+    private long id;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private boolean enabled;
+    private String username;
+
+
+
+
+
+}
